36c2cb683d8f64f44e096a70dbf1f4b553b6d401
zleonwa <zleonwa@gmail.com>
2018-08-08 21:18:18 +0900

    탭 과제

---
+++ MySampleTab/app/src/main/java/com/example/dami/mysampletab/Fragment3.java
@@ -0,0 +1,21 @@
+package com.example.dami.mysampletab;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Fragment3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment3, container, false);
+        return rootView;
+    }
+}
